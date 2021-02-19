28250f37fccf532dee6b2e51a9f67a22ff6d5cd9
Aryant Pratap Singh <46716746+aryantps@users.noreply.github.com>
2021-02-19 10:46:09 +0000

    addedcode for taking input and validating

--- src/main.java
+++ src/main.java
@@ -17,11 +17,22 @@
  */
 
 public class main {
-    public static void(String[] args){
-        try {
-            /*
-            <p></p>
-            */
-        }
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        ArrayList<Details> items = new ArrayList<Details>();
+        Scanner sc = new Scanner(System.in);
+        char choice = 'n';
+        do {
+            System.out.println("Enter new item details:");
+            String input = sc.next();
+            input += sc.nextLine();
+            String[] inpArray = input.split(" ");
+            Details detail = new Details();
+            detail.getItemDetails(inpArray);
+            items.add(detail);
+            System.out.println("Do you want to enter details of other items (y/n):");
+            choice = sc.next().charAt(0);
+        } while (choice == 'y');
+        printDetails(items);
     }
 }
